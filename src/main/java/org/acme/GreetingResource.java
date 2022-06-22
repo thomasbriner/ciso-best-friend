12517f271019c3317bbe13d85ee524068e42b412
Thomas Briner <thomas.briner@ergon.ch>
2022-06-22 14:41:11 +0200

    use different endpoints

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,16 +1,37 @@
 package org.acme;
 
+import io.quarkus.security.Authenticated;
+
+import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/hello")
+@Path("/")
 public class GreetingResource {
 
-    @GET
+    @GET()
+    @Path("welcome")
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from RESTEasy Reactive";
+    public String welcome() {
+        return "Public";
     }
+
+    @GET
+    @Path("user")
+    @Authenticated
+    @Produces(MediaType.TEXT_PLAIN)
+    public String user() {
+        return "user";
+    }
+
+    @GET
+    @Path("admin")
+    @RolesAllowed("admin")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String admin() {
+        return "admin";
+    }
+
 }
